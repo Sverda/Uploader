162bdd01cd9d903775e264e7193f8162df9f9722
Damian Ubowski <damian.ubowski.0@gmail.com>
2021-02-17 13:42:34 +0100

    Add README.md

---
+++ README.md
@@ -0,0 +1,24 @@
+# Uploader - File Uploading Web Application
+Uploader is Single Page Web Application created to try my skills in React JS and .NET 5 technologies. 
+Uploader only function is to upload files through web api to SQL database. 
+Project Architecture is based on Jason Tyler's Clean Architecture approach. 
+
+## Get Started
+Go to *src/WebUI* and set user secrets. 
+> cd src/WebUI
+
+> dotnet user-secrets set "Db:UserId" "<YOUR_USERID>
+
+> dotnet user-secrets set "Db:Password" "<YOUR_PASSWORD>
+
+Create database. 
+> dotnet ef database update
+
+Run application (you have to be in *src/WebUI*). 
+> dotnet run
+
+## Demo
+![select-file](docs/images/select-file.jpg)
+![in-progress](docs/images/progress.jpg)
+![error](docs/images/error.jpg)
+![success](docs/images/success.jpg)
--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -3,6 +3,7 @@ using Uploader.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Data.SqlClient;
 
 namespace Uploader.Infrastructure
 {
@@ -10,9 +11,14 @@ namespace Uploader.Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
+            var builder = new SqlConnectionStringBuilder(
+                configuration.GetConnectionString("DefaultConnection"));
+            builder.UserID = configuration["Db:UserId"];
+            builder.Password = configuration["Db:Password"];
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    configuration.GetConnectionString("UploaderSecrets:ConnectionString"),
+                    builder.ConnectionString,
                     b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
 
             services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
